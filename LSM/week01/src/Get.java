09d43b11515c4e01c14225c06a063658607fd1b9
tkdalsss <lsm991229@naver.com>
2024-03-18 00:14:45 +0900

    feat: http GET method 통신 코드
    http GET method 통신 코드 구현

--- LSM/week01/src/Get.java
+++ LSM/week01/src/Get.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Get {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Get {
+    static final String URL = "https://www.google.com";
+    static final String USER_AGENT = "Mozilla/5.0";
+    static final String DATA = "get data";
+    public static void main(String[] args) throws IOException {
+        URL url = new URL(URL);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = conn.getResponseCode();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuffer sb = new StringBuffer();
+        String inputLine;
+
+        while ((inputLine = br.readLine()) != null) {
+            sb.append(inputLine);
+        }
+        br.close();
+
+        String response = sb.toString();
+        System.out.println(response);
+    }
 }
