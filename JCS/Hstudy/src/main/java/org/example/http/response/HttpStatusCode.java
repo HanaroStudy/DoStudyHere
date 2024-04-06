f72c0d79d5ecc10f17fdc4f1739e41c2decedbd1
chansoo <chansoo38@gmail.com>
2024-04-06 15:44:55 +0900

    feat : Http 응답 객체 구현
    feat(ContentType) : ContentType 정의
    feat(HttpStatusCode) : Http 상태 코드 및 메세지 반환기능 추가
    feat(HttpStatusLine) : 응답 첫번째 라인 toString() 정의

--- JCS/Hstudy/src/main/java/org/example/http/response/HttpStatusCode.java
+++ JCS/Hstudy/src/main/java/org/example/http/response/HttpStatusCode.java
@@ -1,15 +1,24 @@
 package org.example.http.response;
 
 public enum HttpStatusCode {
-    HTTP_OK("200"),
-    HTTP_FOUND("302"),
-    HTTP_BAD_REQUEST("400"),
-    HTTP_UNAUTHORIZED("403"),
-    HTTP_NOT_FOUND("404"),
-    HTTP_METHOD_NOT_ALLOWED("405");
+    HTTP_OK("200","OK"),
+    HTTP_FOUND("302", "FOUND"),
+    HTTP_BAD_REQUEST("400", "BAD_REQUEST"),
+    HTTP_UNAUTHORIZED("403", "UNAUTHORIZED"),
+    HTTP_NOT_FOUND("404", "NOT_FOUND"),
+    HTTP_METHOD_NOT_ALLOWED("405", "NOT_ALLOWED");
     private final String code;
-
-    HttpStatusCode(String code) {
+    private final String DEFAULT_MESSAGE;
+    HttpStatusCode(String code, String defaultMessage) {
         this.code = code;
+        this.DEFAULT_MESSAGE = defaultMessage;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDefaultMessage() {
+        return DEFAULT_MESSAGE;
     }
 }
--- JCS/Hstudy/src/main/java/org/example/http/response/HttpStatusLine.java
+++ JCS/Hstudy/src/main/java/org/example/http/response/HttpStatusLine.java
@@ -1,4 +1,23 @@
 package org.example.http.response;
 
 public class HttpStatusLine {
+    private final String HTTP_VERSION = "HTTP/1.1";
+    private final HttpStatusCode HTTP_STATUS;
+
+    public static HttpStatusLine of(HttpStatusCode statusCode) {
+        return new HttpStatusLine(statusCode);
+    }
+
+    private HttpStatusLine(HttpStatusCode statusCode) {
+        this.HTTP_STATUS = statusCode;
+
+    }
+
+    @Override
+    public String toString() {
+        return String.join(" ",
+                HTTP_VERSION,
+                HTTP_STATUS.getCode(),
+                HTTP_STATUS.getDefaultMessage());
+    }
 }
